fe87550bfd26f1051014593799397f6725ab7b71
DragonSenseiGuy <200907890+DragonSenseiGuy@users.noreply.github.com>
2025-11-28 17:43:39 -0500

    Add the `xkcd-fetch` command

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from discord import app_commands
 from discord.ext import commands
 import os
 from dotenv import load_dotenv
+import aiohttp
+from discord import Embed
 
 load_dotenv()
 
@@ -59,6 +61,40 @@ async def delete_text_channel(ctx: discord.Interaction, channel_id: str):
         await ctx.response.send_message("I don't have permission to delete channels here.")
     except Exception as e:
         await ctx.response.send_message(f"An error occurred: {e}")
+
+# xkcd
+@bot.tree.command(name="xkcd-fetch", description="Fetches a specific xkcd")
+@app_commands.describe(xkcd_id="The id of the xkcd")
+async def xkcd_fetch(ctx: discord.Interaction, xkcd_id: str):
+    """Fetches a specific xkcd."""
+    try:
+        async with aiohttp.ClientSession() as session:
+            async with session.get(f"https://xkcd.com/{xkcd_id}/info.0.json") as response:
+                if response.status != 200:
+                    embed = Embed(title="Error", description=f"Could not retrieve xkcd comic #{xkcd_id}.", colour=0xCD6D6D)
+                    await ctx.response.send_message(embed=embed)
+                    return
+
+                info = await response.json()
+
+        embed = Embed(title=f"XKCD comic #{info['num']}", url=f"https://xkcd.com/{info['num']}", colour=0x68C290)
+        embed.description = info["alt"]
+        date = f"{info['year']}/{info['month']}/{info['day']}"
+        embed.set_footer(text=f"{date} - #{info['num']}, '{info['safe_title']}'")
+
+        if info["img"].endswith(("jpg", "png", "gif")):
+            embed.set_image(url=info["img"])
+        else:
+            embed.description = (
+                "The selected comic is interactive, and cannot be displayed within an embed.\n"
+                f"Comic can be viewed [here](https://xkcd.com/{info['num']})."
+            )
+
+        await ctx.response.send_message(embed=embed)
+
+    except Exception as e:
+        embed = Embed(title="Error", description=f"An error occurred: {e}", colour=0xCD6D6D)
+        await ctx.response.send_message(embed=embed)
 
 # Sync
 @bot.tree.command(name="sync", description="Syncs the slash commands to Discord.")

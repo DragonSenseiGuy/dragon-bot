b6d409394530aa580d4ecfadae9b7b08bca44bbb
DragonSenseiGuy <200907890+DragonSenseiGuy@users.noreply.github.com>
2025-11-29 18:30:57 -0500

    Add the `fool` command

--- cogs/fun.py
+++ cogs/fun.py
@@ -2,6 +2,11 @@ from discord import app_commands
 from discord.ext import commands
 from typing import Literal
 import pyjokes
+import random
+from json import loads
+from pathlib import Path
+
+ALL_VIDS = loads(Path("resources/fun/april_fools_vids.json").read_text("utf-8"))
 
 class Fun(commands.Cog):
     def __init__(self, bot: commands.Bot):
@@ -13,6 +18,15 @@ class Fun(commands.Cog):
         """Retrieves a joke of the specified `category` from the pyjokes api."""
         joke = pyjokes.get_joke(category=category)
         await ctx.response.send_message(joke)
+
+    @app_commands.command(name="fool", description="Get a random April Fools' video from Youtube.")
+    async def april_fools(self, ctx: commands.Context) -> None:
+        """Get a random April Fools' video from Youtube."""
+        video = random.choice(ALL_VIDS)
+
+        channel, url = video["channel"], video["url"]
+
+        await ctx.response.send_message(f"Check out this April Fools' video by {channel}.\n\n{url}")
 
 async def setup(bot: commands.Bot):
     await bot.add_cog(Fun(bot))
